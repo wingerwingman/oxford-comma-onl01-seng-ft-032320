1b0da285edf0b7fb054685d4affe161268b148e1
Justin Winger <justinwinger4@gmail.com>
2020-03-25 14:50:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   index = array.length - 1
   array.join(", ")
   array2 = index
-  array2.unshift("and ").join()
+  array2.unshift("and ")
+  array2.join()
   array.slice(index).push(array2)
 end
